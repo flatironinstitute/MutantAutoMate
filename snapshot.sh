8c4b12e4807c9a3d4c523b7c9c36b4b844b576d3
akshadapradhan <116102437+akshadapradhan@users.noreply.github.com>
2023-04-26 12:24:13 -0400

    Add files via upload
    Final Snapshot

--- snapshot.sh
+++ snapshot.sh
@@ -4,38 +4,30 @@
 # Get file paths from user input
 file1="$1"
 file2="$2"
-
-res='131-141'
-name='2Z6G'
-name2='New_2x6g'
-echo ${file1:32:4}
-echo $name
+echo $file2
+echo ${file2:32:5}
 
 #create the script
-echo "load $file1
-set cartoon_fancy_helices = 1
-set cartoon_highlight_color = grey70
-bg_color WHITE
-set antialias = 1
-set ortho = 1
-set sphere_mode, 5
+echo "bg_color white  
 
-colour red, 2Z6G
-show cartoon, 2Z6G
+load $file1
+colour blue, ${file1:32:4}
 
-select res $res
-remove res $res
-
-ray 1000,1500
 load $file2
-colour red, New_2x6g
-align ${file1:32:4}, New_2x6g
+color red, ${file2:32:5}
+
+align ${file1:32:4}, ${file2:32:5}
+
 set label_size, -3
+set label_color, black
 set label_font_id, 10
 pseudoatom foo
-set label_position,(3,2,1)
-label foo, 'removed part'
-png New_2x6g.png" > try1.pml
+set label_position,(50, 10, 10)
+label foo, 'TRY'
+set ray_opaque_background, 1
+ray
+png 3.png, width=800, height=600, dpi=300
+quit" > try1.pml
 
 # GO!
 pymol -c try1.pml
