ca5c9e80da18eeec554f05fd98c489036599921f
akshadapradhan <116102437+akshadapradhan@users.noreply.github.com>
2023-06-21 12:04:43 -0400

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
-# Get file paths from user input
+# Get file path and residue number from user input
 file1="$1"
+residue_number="$2"
 
 # Extract the name of the PDB file
 pdb_file=$(basename "$file1")
+echo "$residue_number"
 
 # Create the script
 echo "bg_color white
 
 load $file1
 
-# Select the residue and color it differently
-select resi 100
-color red, resi 100
+# Color the whole protein green
+color green
 
-# Zoom into the selected residue
-zoom resi 100
+# Select the residue and color it red
+select resi ${residue_number}
+color red, res ${residue_number}
+
+#cartoon for residue and sticks for protein
+cartoon oval, res ${residue_number}
+show sticks
+
+#rotate for better visibility
+rotate [1,1,1], ${residue_number}, chain A
 
 set label_size, -3
 set label_color, black
 set label_font_id, 10
 pseudoatom foo
 set label_position,(50, 10, 10)
-label foo, 'TRY'
+label foo, '${file1: (-8)}'
 set ray_opaque_background, 1
 ray
-png 3.png, width=800, height=600, dpi=300
+png snap.png, width=800, height=600, dpi=300
 quit" > try1.pml
 
 # Run the script with PyMOL
