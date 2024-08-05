0428fb17c7bc434f25746ea180809fc38c940bd4
Paul Murray <plmrry@gmail.com>
2024-08-05 13:53:32 -0400

    more process stuff

--- src/mutantautomate/process.py
+++ src/mutantautomate/process.py
@@ -1,4 +1,5 @@
 import re
+import ast
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
@@ -9,6 +10,43 @@ from Bio.Align import PairwiseAligner
 retries = Retry(total=5, backoff_factor=0.25, status_forcelist=[500, 502, 503, 504])
 session = requests.Session()
 session.mount("https://", HTTPAdapter(max_retries=retries))
+
+# Define the dictionary of amino acid names
+amino_acids = {
+    "A": "Alanine",
+    "C": "Cysteine",
+    "D": "Aspartic Acid",
+    "E": "Glutamic Acid",
+    "F": "Phenylalanine",
+    "G": "Glycine",
+    "H": "Histidine",
+    "I": "Isoleucine",
+    "K": "Lysine",
+    "L": "Leucine",
+    "M": "Methionine",
+    "N": "Asparagine",
+    "P": "Proline",
+    "Q": "Glutamine",
+    "R": "Arginine",
+    "S": "Serine",
+    "T": "Threonine",
+    "V": "Valine",
+    "W": "Tryptophan",
+    "Y": "Tyrosine",
+}
+
+
+def calculate_grantham_score(residue1, residue2):
+    # Load the Grantham dictionary from the output file
+    output_file = "grantham_output.txt"
+    with open(output_file, "r") as f:
+        grantham_dict = ast.literal_eval(f.read())
+    key = (residue1, residue2)
+    if key in grantham_dict:
+        return grantham_dict[key]
+    else:
+        print(f"Grantham score not available for ({aa1}, {aa2})")
+        return None
 
 
 def construct_uniprot_search_url(gene_name):
@@ -117,28 +155,41 @@ def calculate_similarity(sequence1, sequence2):
     aligner.match_score = 1
     aligner.mismatch_score = -1
     alignments = aligner.align(sequence1, sequence2)
-    print(f"number of alignments: {len(alignments)}")
     best_alignment = alignments[0]
-    # print("best alignment:")
-    # print(best_alignment)
     alignment_score = best_alignment.score
-    print(f"alignment_score: {alignment_score}")
-    alignment_length = len(best_alignment)
-    print(f"alignment_length: {alignment_length}")
-    print(f"sequence1 length: {len(sequence1)}")
-    print(f"sequence2 length: {len(sequence2)}")
-    similarity = alignment_score / alignment_length * 100
-    return similarity
+    return alignment_score
+    # alignment_length = len(best_alignment)
+    # print(f"alignment_length: {alignment_length}")
+    # print(f"sequence1 length: {len(sequence1)}")
+    # print(f"sequence2 length: {len(sequence2)}")
+    # similarity = alignment_score / alignment_length * 100
+    # return similarity
 
 
 def process(gene_name, residue1, position, residue2, top_isoforms):
-    """Main entry point for the script."""
-    print("Call your main application code here")
     print("Gene Name:", gene_name)
     print("Residue 1:", residue1)
     print("Position:", position)
     print("Residue 2:", residue2)
     print("Top Isoforms:", top_isoforms)
+
+    # Grantham Score
+    grantham_score = calculate_grantham_score(residue1, residue2)
+    grantham_threshold = 100
+    yield {
+        "message": f"Grantham score: {grantham_score}",
+        "grantham_score": grantham_score,
+    }
+    if grantham_score is not None:
+        yield {
+            "message": f"The Grantham score between {amino_acids.get(residue1)} and {amino_acids.get(residue2)} is {grantham_score}"
+        }
+        if grantham_score > grantham_threshold:
+            yield {
+                "message": f"This is a high Grantham score, indicating a potentially significant evolutionary distance."
+            }
+        else:
+            yield {"message": f"This is not a high Grantham score."}
 
     # Collect all isoforms
     all_isoforms = {}
@@ -149,7 +200,7 @@ def process(gene_name, residue1, position, residue2, top_isoforms):
                 "sequence": None,
                 "gene_name": None,
             }
-        yield f"got {len(all_isoforms)} isoforms"
+        yield {"message": f"got {len(all_isoforms)} isoforms"}
 
     # Collect all sequences
     sequences_found = 0
@@ -158,8 +209,8 @@ def process(gene_name, residue1, position, residue2, top_isoforms):
         sequence = result["sequence"]
         all_isoforms[isoform]["sequence"] = sequence
         sequences_found += 1
-        yield f"got sequence for {isoform}"
-        yield f"got {sequences_found} / {len(all_isoforms)} sequences"
+        yield {"message": f"got sequence for {isoform}"}
+        yield {"message": f"got {sequences_found} / {len(all_isoforms)} sequences"}
 
     # Find isoforms with residue1 at position
     matching_isoforms = {}
@@ -167,66 +218,44 @@ def process(gene_name, residue1, position, residue2, top_isoforms):
         sequence = data["sequence"]
         if search_residue(sequence, residue1, position):
             matching_isoforms[isoform] = data
-            yield f"found {isoform} with {residue1} at position {position}"
-    yield f"found {len(matching_isoforms)} matching isoforms: {matching_isoforms.keys()}"
+            yield {"message": f"found {isoform} with {residue1} at position {position}"}
+    yield {
+        "message": f"found {len(matching_isoforms)} matching isoforms",
+        "matching_isoforms": matching_isoforms.keys(),
+    }
 
     # Get gene names for matching isoforms
     for result in get_gene_names_generator(matching_isoforms.keys()):
         isoform = result["isoform"]
         gene_name = result["gene_name"]
         all_isoforms[isoform]["gene_name"] = gene_name
-        yield f"got gene name for {isoform}: {gene_name}"
+        yield {"message": f"got gene name for {isoform}", "gene_name": gene_name}
 
     # Filter isforms that match the input gene name
     filtered_isoforms = {}
     for isoform in matching_isoforms:
-        print(f"isoform: {isoform}")
         this_gene_name = matching_isoforms[isoform]["gene_name"]
-        print(f"this_gene_name: {this_gene_name}")
         if this_gene_name == gene_name:
-            print(f"it matches {isoform} {this_gene_name}")
             filtered_isoforms[isoform] = matching_isoforms[isoform]
-        else:
-            print(f"it doesn't match {isoform} {this_gene_name}")
-    yield f"filtered isoforms: {filtered_isoforms.keys()}"
+    yield {
+        "message": f"filtered isoforms: {filtered_isoforms.keys()}",
+        "filtered_isoforms": filtered_isoforms.keys(),
+    }
 
     # Pairwise similary scores
     pairwise_scores = {}
     for isoform1 in filtered_isoforms:
-        print(f"isoform1: {isoform1}")
         for isoform2 in filtered_isoforms:
-            print(f"isoform2: {isoform2}")
             sequence1 = filtered_isoforms[isoform1]["sequence"]
             sequence2 = filtered_isoforms[isoform2]["sequence"]
-            similarity = calculate_similarity(sequence1, sequence2)
-            pairwise_scores[(isoform1, isoform2)] = similarity
-            yield f"similarity for {isoform1} and {isoform2}: {similarity}"
-
-    print(pairwise_scores)
-
-    # TODO: Is this right?
-    # Get the sequence of the main gene?
-    # input_gene_sequence = get_sequence(gene_name)
-    # print(f"input gene sequence: {input_gene_sequence}")
-
-    # Score isoforms by similarity
-    # for isoform, data in filtered_isoforms:
-    #     sequence = data["sequence"]
-    #     similarity = calculate_similarity(sequence, sequence)
-    #     yield f"similarity for {isoform}: {similarity}"
-
-    # # Score isoforms by similarity
-    # scored_isoforms = []
-    # for isoform in filtered_isoforms:
-    #     # Get the sequence from all_sequences
-    #     sequence = next(
-    #         (seq for isoform_id, seq in all_sequences if isoform_id == isoform), None
-    #     )
-
-    # # Test similartity
-    # similarty = calculate_similarity(all_sequences[0][1], all_sequences[1][1])
-    # yield f"similarity: {similarty}"
-    # print("done")
+            alignment_score = calculate_similarity(sequence1, sequence2)
+            pairwise_scores[(isoform1, isoform2)] = alignment_score
+            yield {
+                "message": f"similarity for {isoform1} and {isoform2}: {alignment_score}"
+            }
+    yield {
+        "pairwise_scores": pairwise_scores,
+    }
 
 
 def main():
@@ -257,3 +286,28 @@ main()
 # for result in get_gene_names_generator(matching_isoforms):
 #     print(f"got gene name: {result}")
 #     isoforms_with_gene_names.append((result["isoform"], result["gene_name"]))
+
+
+# TODO: Is this right?
+# Get the sequence of the main gene?
+# input_gene_sequence = get_sequence(gene_name)
+# print(f"input gene sequence: {input_gene_sequence}")
+
+# Score isoforms by similarity
+# for isoform, data in filtered_isoforms:
+#     sequence = data["sequence"]
+#     similarity = calculate_similarity(sequence, sequence)
+#     yield f"similarity for {isoform}: {similarity}"
+
+# # Score isoforms by similarity
+# scored_isoforms = []
+# for isoform in filtered_isoforms:
+#     # Get the sequence from all_sequences
+#     sequence = next(
+#         (seq for isoform_id, seq in all_sequences if isoform_id == isoform), None
+#     )
+
+# # Test similartity
+# similarty = calculate_similarity(all_sequences[0][1], all_sequences[1][1])
+# yield f"similarity: {similarty}"
+# print("done")
