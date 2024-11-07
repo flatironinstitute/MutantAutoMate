5019be97cd1f5f0c722dd9600f210bfccecd4056
Paul Murray <plmrry@gmail.com>
2024-11-07 14:47:20 -0500

    update run-with-docker.sh

--- run-with-docker.sh
+++ run-with-docker.sh
@@ -1,9 +1,14 @@
 # docker build -t mutantautomate .
-module load docker
-docker run -it --rm \
-  -v $(pwd):/usr/src/app \
-  -w /usr/src/app \
-  -e FLASK_ENV=development \
-  -p 5000:5000 \
+# module load docker
+docker \
+  run \
+  --rm \
+  --detach \
+  --name mutantautomate \
+  --volume $(pwd):/usr/src/app \
+  --publish 5000:5000 \
+  --workdir /usr/src/app \
+  --env FLASK_ENV=development \
+  --workdir /usr/src/app \
   mutantautomate \
   flask --app src/mutantautomate/app2 run --host=0.0.0.0 --debug
